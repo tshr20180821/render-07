02cc58a95da0071b7ce726c986f31f3672f08c65
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-28 08:19:27 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,14 @@
 
 set -x
 
-ls -lang /usr/src/app/gnupg/bin
-cp /usr/src/app/gnupg/bin/gpg /var/www/html/
+# ls -lang /usr/src/app/gnupg/bin
+# cp /usr/src/app/gnupg/bin/gpg /var/www/html/
 
 whereis apache2
+find / -name envvars -print
 
-echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
-. /etc/apache2/envvars
+# echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
+# . /etc/apache2/envvars
 
-exec /usr/sbin/apache2 -DFOREGROUND
+# exec /usr/sbin/apache2 -DFOREGROUND
+exec apache2 -DFOREGROUND
