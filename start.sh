1d0b4ee0696b62faeb01d49ec29f5c2191743e92
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-12 09:40:58 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,8 +9,6 @@ ls -lang
 ls -lang /usr/local/apache2/htdocs/
 /usr/local/apache2/htdocs/gpg --version
 
-ls -lang /usr/local/apache2/conf/
-
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >>/usr/local/apache2/conf/httpd.conf
 
 . /usr/local/apache2/bin/envvars
