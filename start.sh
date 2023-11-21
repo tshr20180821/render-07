a6f656918bd6ca877113c803e43ba66c98907768
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-21 16:13:43 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,13 @@ dpkg -l
 export DEPLOY_DATETIME=$(date +'%Y%m%d%H%M%S')
 export FIXED_THREAD_POOL=1
 
+curl -O https://raw.githubusercontent.com/tshr20180821/render-04/main/auth/exec_log_operation.php
 curl -O https://raw.githubusercontent.com/tshr20180821/render-04/main/app/log.php
 echo 'function apcu_store($dummy1, $dummy2) {}' >>./log.php
-TEST_FILE_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-cp ./test.php /var/www/html/${TEST_FILE_NAME}.php
+TEST_FILE_NAME_1=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+cp ./test.php /var/www/html/${TEST_FILE_NAME_1}.php
+TEST_FILE_NAME_2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+cp ./exec_log_operation.php /var/www/html/${TEST_FILE_NAME_2}.php
 ls -lang
 
 touch /var/www/html/index.html
@@ -22,6 +25,6 @@ export SQLITE_LOG_DB_FILE="/tmp/sqlitelog.db"
 
 . /etc/apache2/envvars >/dev/null 2>&1
 
-sleep 5s && ps aux && curl -m 30 https://${RENDER_EXTERNAL_HOSTNAME}/${TEST_FILE_NAME}.php &
+sleep 5s && ps aux && curl -m 60 https://${RENDER_EXTERNAL_HOSTNAME}/${TEST_FILE_NAME_1}.php &
 
 exec /usr/sbin/apache2 -DFOREGROUND
