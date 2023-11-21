5d4fae258bce4a02c7e95e621a7c894d98cd9619
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-21 16:03:42 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,6 +22,6 @@ export SQLITE_LOG_DB_FILE="/tmp/sqlitelog.db"
 
 . /etc/apache2/envvars >/dev/null 2>&1
 
-sleep 5s && ps aux && curl https://${RENDER_EXTERNAL_HOSTNAME}/${TEST_FILE_NAME}.php &
+sleep 5s && ps aux && curl -m 30 https://${RENDER_EXTERNAL_HOSTNAME}/${TEST_FILE_NAME}.php &
 
 exec /usr/sbin/apache2 -DFOREGROUND
