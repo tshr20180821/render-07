a2fe715a3faec8bbed207a01f560b8f7a7ac7723
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-21 16:18:21 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ export FIXED_THREAD_POOL=1
 curl -O https://raw.githubusercontent.com/tshr20180821/render-04/main/auth/exec_log_operation.php
 curl -O https://raw.githubusercontent.com/tshr20180821/render-04/main/app/log.php
 echo 'function apcu_store($dummy1, $dummy2) {}' >>./log.php
+echo 'function apcu_exists($dummy1) {}' >>./log.php
 TEST_FILE_NAME_1=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 cp ./test.php /var/www/html/${TEST_FILE_NAME_1}.php
 TEST_FILE_NAME_2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
