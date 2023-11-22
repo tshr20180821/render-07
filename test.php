1a03e7f36791d60c8e3940d8a6dda2b33555885f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-22 09:01:47 +0900

    Update test.php

--- test.php
+++ test.php
@@ -2,9 +2,11 @@
 
 include('/usr/src/app/log.php');
 
-error_log('START test.php');
+error_log('test.php START');
 
 $log = new Log();
+
+error_log('test.php CHECK POINT 010');
 
 $log->trace('php Log Level Test : trace');
 $log->debug('php Log Level Test : debug');
@@ -12,3 +14,5 @@ $log->info('php Log Level Test : info');
 $log->warn('php Log Level Test : warn');
 $log->error('php Log Level Test : error');
 $log->fatal('php Log Level Test : fatal');
+
+error_log('test.php FINISH');
