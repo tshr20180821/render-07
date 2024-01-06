de12b386713de430ef2a0b3c960698423edf7cbc
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-07 06:00:02 +0900

    Update build_ssh.sh

--- build_ssh.sh
+++ build_ssh.sh
@@ -7,7 +7,7 @@ export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
 apt-get -qq update
-apt-get install zlib1g-dev libcrypto++8
+apt-get install zlib1g-dev libssl-dev
 
 pushd /tmp
 curl -LO https://github.com/rapier1/hpn-ssh/archive/refs/tags/18.2.0.tar.gz
@@ -18,7 +18,7 @@ pushd hpn-ssh-18.2.0
 autoreconf
 ./configure --help
 ./configure --prefix=/tmp/usr --with-pam --with-ipaddr-display
-
+time make -j4
 popd
 
 popd
