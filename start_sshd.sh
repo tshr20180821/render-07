1202562d0d8122a13da6bae4fc78772c9c880280
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-08 06:32:47 +0900

    Update start_sshd.sh

--- start_sshd.sh
+++ start_sshd.sh
@@ -7,13 +7,32 @@ cat /etc/ssh/sshd_config
 curl -Lo /tmp/hpnsshd https://raw.githubusercontent.com/tshr20180821/render-07/main/app/hpnsshd
 chmod +x /tmp/hpnsshd
 
-ls -lang /tmp/
-
-mkdir /app/.ssh
+mkdir -p /app/.ssh
 chmod 700 /app/.ssh
 
 ssh-keygen -t rsa -N '' -f /app/.ssh/ssh_host_rsa_key
 
-/tmp/hpnsshd -4 -D -p 60022 -h /app/.ssh/ssh_host_rsa_key &
+ls -lang /app/.ssh/
+
+cat << EOF >/tmp/sshd_config
+AddressFamily inet
+ListenAddress 0.0.0.0
+Protocol 2
+PermitRootLogin no
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+PubkeyAuthentication yes
+AuthorizedKeysFile /app/.ssh/ssh_host_rsa_key.pub
+X11Forwarding no
+PrintMotd no
+# LogLevel DEBUG3
+LogLevel VERBOSE
+AcceptEnv LANG LC_*
+PidFile /tmp/sshd.pid
+ClientAliveInterval 120
+ClientAliveCountMax 3
+EOF
+
+/tmp/hpnsshd -4 -D -p 60022 -h /app/.ssh/ssh_host_rsa_key -f /tmp/sshd_config &
 
 sleep 5s && ps aux &
