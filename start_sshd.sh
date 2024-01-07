de5b2f5a0c1c97aa5f7633fcd0a71a1f3caf084d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-08 06:00:08 +0900

    Update start_sshd.sh

--- start_sshd.sh
+++ start_sshd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -Lo /tmp/hpnsshd https://raw.githubusercontent.com/tshr20180821/render-07/main/app/hpnsshd
+chmod +x /tmp/hpnsshd
+
 ssh-keygen -t rsa -N '' -f /tmp/ssh_host_rsa_key
 
 ls -lang /tmp/
