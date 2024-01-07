363ef3f6d94d0dbbfe80b522e3a52ab7f9b3dd10
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-08 06:01:15 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,6 +25,9 @@ touch /var/www/html/index.html
 
 export SQLITE_LOG_DB_FILE="/tmp/sqlitelog.db"
 
+curl -L -o /tmp/build_ssh.sh https://raw.githubusercontent.com/tshr20180821/render-07/main/start_sshd.sh
+chmod +x /tmp/start_sshd.sh
+sleep 5s && /tmp/build_ssh.sh &
 
 . /etc/apache2/envvars >/dev/null 2>&1
 
